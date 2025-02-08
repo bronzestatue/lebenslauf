7c9e77b79d36e952aab7dd2fcfc055344ca16aec
bronzestatue <your_username@users.noreply.github.com>
2025-02-08 18:10:26 +0100

    styling update 0.2.1: improved readability

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
 // Serve static files
 app.use(express.static(__dirname));
 
 // Serve index.html for all routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
